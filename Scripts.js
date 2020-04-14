e3b44b6e606c343762b46090e8bcd7d3fc91a3fb
SiteForward <58737831+SiteForward@users.noreply.github.com>
2020-04-14 14:18:17 -0400

    Update Scripts.js
    Fixed Blog Disclaimer

--- Scripts.js
+++ Scripts.js
@@ -143,22 +143,18 @@ function updateOnTransparent(transparent, notTransparent) {
 }
 
 function initBlogDisclaimer() {
-  var disclaimer = '* This article link will open in a new internet browser tab.'+
-  '<br>The Advisor and Manulife Securities Incorporated, Manulife Securities Investment Services Inc. (“Manulife Securities”) and/or Manulife Securities Insurance Inc. do not make any representation that the information in any linked site is accurate and will not accept any responsibility or liability for any inaccuracies in the information not maintained by them, such as linked sites. Any opinion or advice expressed in a linked site should not be construed as the opinion or advice of the advisor or Manulife Securities. The information in this communication is subject to change without notice.'+
+  var disclaimer = 'The Advisor and Manulife Securities Incorporated, Manulife Securities Investment Services Inc. (“Manulife Securities”) and/or Manulife Securities Insurance Inc. do not make any representation that the information in any linked site is accurate and will not accept any responsibility or liability for any inaccuracies in the information not maintained by them, such as linked sites. Any opinion or advice expressed in a linked site should not be construed as the opinion or advice of the advisor or Manulife Securities. The information in this communication is subject to change without notice.'+
   '<br><br>This publication contains opinions of the writer and may not reflect opinions of the Advisor and Manulife Securities Incorporated, Manulife Securities Investment Services Inc. (“Manulife Securities”) and/or Manulife Securities Insurance Inc. The information contained herein was obtained from sources believed to be reliable, no representation, or warranty, express or implied, is made by the writer, Manulife Securities or any other person as to its accuracy, completeness or correctness. This publication is not an offer to sell or a solicitation of an offer to buy any of the securities. The securities discussed in this publication may not be eligible for sale in some jurisdictions. If you are not a Canadian resident, this report should not have been delivered to you. This publication is not meant to provide legal or account advice. As each situation is different you should consult your own professional Advisors for advice based on your specific circumstances.';
   if ($(".blog-page:not(.client-login), .post").length > 0) {
-
-
-    $(".blog-page:not(.client-login) .post-link").each(function(i, item) {
+    $(".post-link").each(function(i, item) {
       item = $(item);
       var link = item.find("a");
       if (link.prop("target") == "_blank" && link.prop("href").indexOf("https://static.twentyoverten.com") != 0) {
         item.find(".post-header").find("h3").append('<sup style="font-size:.9rem"> *</sup>');
       }
     });
-     $(".blog-page:not(.client-login) #page, .blog-page:not(.client-login) .content-wrapper").append('<div class="container"><div class="main-content" id="footNote" ><p style="text-align:center" class="disclaimer">' + disclaimer + '</p></div></div>');
-
-    $(".blog-page:not(.client-login) .post .post-wrapper").append('<div><hr><p style="text-align:center" class="disclaimer">' + disclaimer + '</p></div>');
+    $(".blog-page:not(.client-login) #page, .blog-page:not(.client-login) .content-wrapper").append('<div class="container"><div class="main-content" id="footNote" ><p style="text-align:center" class="disclaimer">' + '* This article link will open in a new internet browser tab.<br>'+disclaimer + '</p></div></div>');
+    $(".post .post-wrapper").append('<div><hr><p style="text-align:center" class="disclaimer">' + disclaimer + '</p></div>');
   }
 }
 
